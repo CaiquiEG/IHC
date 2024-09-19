c84cf0f1b322309ab37d3a102bc53f8c75bfc16e
Caiqui Estrela Guedes <134959733+CaiquiEG@users.noreply.github.com>
2024-09-19 15:05:04 -0300

    calculadora

---
+++ pack/SRC/index.js
@@ -0,0 +1,23 @@
+function performCalculation() {
+    var num1 = parseFloat(document.getElementById('input1').value);
+    var num2 = parseFloat(document.getElementById('input2').value);
+    var operation = document.getElementById('operation').value;
+    var result;
+
+    switch (operation) {
+        case 'add':
+            result = num1 + num2;
+            break;
+        case 'subtract':
+            result = num1 - num2;
+            break;
+        case 'multiply':
+            result = num1 * num2;
+            break;
+        case 'divide':
+            result = num1 / num2;
+            break;
+    }
+
+    document.getElementById('result').textContent = 'Resultado: ' + result;
+}
